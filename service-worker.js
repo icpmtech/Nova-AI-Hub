83ac74e66fb265cb595a8535d608f41faaf38be4
Pedro Martins <mourao.martins@gmail.com>
2025-04-21 16:43:14 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,8 @@ const FILES_TO_CACHE = [
   '/dashboard.html',
   '/manifest.json',
   '/service-worker.js',
-  '/styles.css', // if you extract inline CSS
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', evt => {
